c7c82c2eb219bdb3575287e6566c5f7318c3c42d
Bao <nguyengiabao30092002@gmail.com>
2024-11-02 15:57:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-500 flex items-center justify-center rounded-[1rem] m-[5rem]">
+      <Button>ban la j</Button>
+    </div>
+  );
+}
+
+export default App;
