b63155fc4f24ffcf6da43b1f056b9ff1fa558cce
Bao <nguyengiabao30092002@gmail.com>
2024-12-15 21:01:59 +0700

    update

--- src/layouts/Admin/Dashboard.jsx
+++ src/layouts/Admin/Dashboard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "@/components/HeaderAdmin";
 import Dashboard from "@/pages/AdminPages/Dashboard";
-import Test from "@/components/TestAPI";
 
 export default function AdminLayout() {
   return (
     <div>
       <Header />
-      <Test />
+      <Dashboard />
     </div>
   );
 }
